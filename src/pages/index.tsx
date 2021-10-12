a1dfa8389f527ec6f8a52d5c3ba8e669aa434922
Arielle Silva <arielle95@gmail.com>
2021-10-12 11:22:35 -0300

    Add UserInformation files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,29 @@
-import type { NextPage } from 'next';
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import type { NextPage } from "next";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 const Home: NextPage = () => {
   return (
     <div>
       <SafeEnvironment />
-      <PageTitle 
-        title={'Conheça os profissionais'} 
-        subtitle={'Preencha o endereço e veja todos os profissionais da sua localidade'}
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha o endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+
+      <UserInformation
+        name={"Arielle"}
+        picture={"https://github.com/arielyth.png"}
+        rating={3}
+        description={"RJ"}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 //todo componente começa com letra maiúscula
---
+++ src/ui/components/data-display/UserInformation/UserInformation.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  UserInformationContainer,
+  UserName,
+  UserDescription,
+  AvatarStyled,
+  RatingStyled,
+} from "./UserInformation.style";
+
+interface UserInformationProps {
+  picture: string;
+  name: string;
+  rating: number;
+  description?: string;
+}
+
+const UserInformation: React.FC<UserInformationProps> = ({
+  name,
+  picture,
+  rating,
+  description,
+}) => {
+  return (
+    <UserInformationContainer>
+      {/* recurso do mui, se não tiver foto aparece a primeira letra*/}
+      <AvatarStyled src={picture}>{name[0]}</AvatarStyled>
+      <UserName>{name}</UserName>
+      <RatingStyled readOnly value={rating} />
+      <UserDescription>{description}</UserDescription>
+    </UserInformationContainer>
+  );
+};
+
+export default UserInformation;
